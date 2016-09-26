a1cc4fddccd6399df3cdf25261aaf50c0d0e6ecc
Emmelia Talarico <emm.talarico@gmail.com>
2016-09-26 12:21:10 -0400

    Added python

---
+++ keys.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#keys.py
+keys = dict(
+ consumer_key =          'ceooAEZ4wBFGBDTkoGBzMxBjN',
+ consumer_secret =       'pP7r7osttnZnx60PEilf6aqHzuKjs6Lzd3uXIqy8X0EXNLiyJ6',
+ access_token =          '768831126097887232-osNkiSROJsKkNKtuNgpWzKbC1ohJURk',
+ access_token_secret =   'kcgTlbf5SwYUzYpbPgroKlQn0EL42T0st0LxFHG5scwcK',
+)
